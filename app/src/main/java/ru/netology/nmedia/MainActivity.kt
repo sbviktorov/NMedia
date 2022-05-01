d14093e3b9508ba3f2a33ea374bb4c1b3285c65d
Sergei Viktorov <sergei_viktorov@mail.ru>
2022-05-01 15:56:38 +0400

    added post layout

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sleep(1000)
+        setContentView(R.layout.post_list_item)
+
     }
 }
